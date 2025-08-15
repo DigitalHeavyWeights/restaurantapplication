bcfd42b2d4cb54d8db8902bccae5f2b31f48121f
travisburns <travisjb0@gmail.com>
2025-08-14 23:14:00 -0700

    added simplified ktichen page

--- src/app/employee/kitchen/page.tsx
+++ src/app/employee/kitchen/page.tsx
@@ -1,41 +1,18 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Header } from '../../components/layout/Header';
 import { TicketQueue } from '../../components/kitchen/TicketQueue';
 import { ProtectedRoute } from '../../components/auth/ProtectedRoute';
-import { useOrderStore } from '../../store/orderStore';
 
 export default function KitchenPage() {
-  const { kitchenQueue, updateOrderStatus, loadKitchenQueue, isLoading } = useOrderStore();
-
-useEffect(() => {
-  console.log('About to load kitchen queue...');
-  loadKitchenQueue().catch(error => {
-    console.error('Kitchen queue error:', error);
-  });
-}, []);
-
   return (
     <ProtectedRoute requiredRoles={['Employee', 'Manager']}>
       <div className="pb-20">
         <Header title="Kitchen Queue" showNotifications />
         
         <div className="p-4">
-    {isLoading ? (
-  <div>Loading orders...</div>
-) : kitchenQueue.length > 0 ? (
-  kitchenQueue.filter(order => order != null).map((order) => (  // Add filter
-    <TicketQueue
-      key={order.orderId}
-      order={order}
-      onStatusUpdate={updateOrderStatus}
-      autoRefresh={true}
-      refreshInterval={15000}
-    />
-  ))
-) : (
-  <div>No orders in queue</div>
-)}
+          {/* TicketQueue handles everything - fetching, filtering, displaying */}
+          <TicketQueue autoRefresh refreshInterval={15000} />
         </div>
       </div>
     </ProtectedRoute>
